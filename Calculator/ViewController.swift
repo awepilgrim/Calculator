45c5fa4658c247666246a6d358ebefb1c72cc9ce
Олег Семёнов <awepilgrim@MacBook-Air-Oleg.local>
2022-09-09 22:02:07 +0400

    initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Олег Семёнов on 09.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+}
+
